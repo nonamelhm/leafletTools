458a6a67b3a3cea5251e796c2d79d43c27873c2e
lhm <2628764605@qq.com>
2023-03-19 18:09:03 +0800

    refactor:更改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
     <header style="text-align:center;font-size:24px;font-weight:700;">Leaflet地图功能封装</header>
-    <keep-alive><router-view /> </keep-alive>
+    <keep-alive> <router-view /> </keep-alive>
   </div>
 </template>
